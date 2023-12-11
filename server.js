2710e8250559c449aeaf4a8bc0d2ec44423b2b6b
WilsonIsCoding <wilsoncheng0704.wc@gmail.com>
2023-12-11 15:07:12 +0800

    [✨ feat]：cors import

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ app.use(cookieParser());
 import cookieParser from "cookie-parser";
 import mongoose from "mongoose";
 import morgan from "morgan";
+import cors from "cors";
 //job-router
 import jobRouter from "./router/jobRouter.js";
 import authRouter from "./router/authRouter.js";
@@ -21,8 +22,22 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 app.use(express.json());
+
+//cors
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+    sameSite: "None"
+  })
+);
+
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+app.get("/api/v1/test", (req, res) => {
+  res.json({ msg: "test route" });
 });
 app.use("/api/v1/jobs", authenticateUser, jobRouter);
 app.use("/api/v1/users", authenticateUser, userRouter);
