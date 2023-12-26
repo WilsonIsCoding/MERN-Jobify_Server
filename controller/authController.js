009520f44eba7cdc073b7b52507f84548dff3ff5
WilsonIsCoding <wilsoncheng0704.wc@gmail.com>
2023-12-26 10:08:02 +0800

    [📄 docs]：readme fix

--- controller/authController.js
+++ controller/authController.js
@@ -12,6 +12,7 @@ export const register = async (req, res) => {
   req.body.password = hashedPassword;
 
   const user = await User.create(req.body);
+  
   res.status(StatusCodes.CREATED).json({ msg: 'user created' });
 };
 export const login = async (req, res) => {
